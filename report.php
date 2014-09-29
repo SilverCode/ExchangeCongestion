4e759fa605595013a8deae86b830e635309ae491
Ross Simpson <ross@onedigitalmedia.com>
2014-09-29 15:20:29 +0200

    Add -h switch to group by hours

--- report.php
+++ report.php
@@ -1,4 +1,33 @@
 <?php
+date_default_timezone_set("Africa/Johannesburg");
+global $__PROGRAM__;
+
+function processFile($filename)
+{
+	$fp = fopen($filename, "r");
+
+	$data = array();
+	$key = "";
+
+	while ($line = fgets($fp))
+	{
+		if (strpos($line, "Start:") === 0)
+		{
+			$date = str_replace("Start: ", "", trim($line));
+			$date = strtotime($date);
+			$key = $date;
+		}
+		else if (strpos($line, "HOST:") === FALSE)
+		{
+			$data[$key][] = trim($line);
+		}
+	}
+
+	$result = processData($data);
+	ksort($result);
+
+	return $result;
+}
 
 function processLine($line)
 {
@@ -32,31 +61,79 @@ function dumpData($data)
 	}
 }
 
-$filename = $argv[1];
-
-$fp = fopen($filename, "r");
-
-$data = array();
-$key = "";
-
-date_default_timezone_set("Africa/Johannesburg");
-
-while ($line = fgets($fp))
+function avg($data)
 {
-	if (strpos($line, "Start:") === 0)
+	$total = 0;
+
+	foreach ($data as $item)
 	{
-		$date = str_replace("Start: ", "", trim($line));
-		$date = strtotime($date);
-		$key = $date;
+		$total += $item;
 	}
-	else if (strpos($line, "HOST:") === FALSE)
+
+	return round($total / count($data));
+}
+
+function getHourlyAvg($data)
+{
+	// Sort the date into hour blocks
+	$tmpoutput = array();
+	foreach ($data as $date=>$ping)
 	{
-		$data[$key][] = trim($line);
+		$hour = date("H:i", $date);
+		$day = date("Y-m-d", $date);
+		$tmpoutput[$hour][$day] = round($ping);
+	}
+
+	ksort($tmpoutput);
+	$output = array();
+
+	foreach ($tmpoutput as $hour=>$pings)
+	{
+		$avg = avg($pings);
+		echo "\"$hour\",$avg\n";
+		$output[$hour] = $avg;
 	}
 }
 
-$result = processData($data);
+function printUsage()
+{
+	global $__PROGRAM__;
+	echo <<<HEREDOC
+Usage: $__PROGRAM__ [--] [options] <file>
+If the program is being invoked from the commandline with PHP
+you need to use -- to prevent PHP from trying to parse more
+command line options.
 
-ksort($result);
+-h		Group by hours
 
-dumpData($result);
+HEREDOC;
+}
+
+function main($argc, $argv)
+{
+	global $__PROGRAM__;
+	$__PROGRAM__ = $argv[0];
+
+	$showHourly = false;
+	$filename = $argv[1];
+
+	if ($argc == 1)
+	{
+		printUsage();
+		exit(1);
+	}
+	else if ($argc == 3 && $argv[1] == "-h")
+	{
+		$showHourly = true;
+		$filename = $argv[2];
+	}
+
+	$data = processFile($filename);
+
+	if ($showHourly)
+		getHourlyAvg($data);
+	else
+		dumpData($data);
+}
+
+main(count($argv), $argv);
